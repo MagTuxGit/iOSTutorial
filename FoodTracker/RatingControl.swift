0546586dc5df006e91d238c505732246cb3bc7d2
Andrij Trubchanin <trand@trandmac.local>
2016-11-16 17:18:03 +0200

    still 5 red buttons, some optimizations (end of a day)

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -22,14 +22,17 @@ class RatingControl: UIView {
     
     var rating = 0
     var ratingButtons = [UIButton]()
+    let spacing = 5
+    let starCount = 5
     
     // MARK: Initialization
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        for _ in 0..<5 {
-            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        for _ in 0..<starCount {
+            let button = UIButton()
+            //let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
             button.backgroundColor = UIColor.red
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), for: .touchDown)
             ratingButtons += [button]
@@ -38,17 +41,23 @@ class RatingControl: UIView {
     }
     
     override func layoutSubviews() {
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
         
         // Offset each button's origin by the length of the button plus spacing.
         for (index, button) in ratingButtons.enumerated() {
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
             button.frame = buttonFrame
         }
     }
     
     override public var intrinsicContentSize: CGSize {
-        return CGSize(width: 240, height: 44)
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        return CGSize(width: width, height: buttonSize)
+        //return CGSize(width: 240, height: 44)
     }
 
     // MARK: Button Action
