ac3f74304887d4cc88b2a3e98dd50c42df3b8202
Andrij Trubchanin <trand@trandmac.local>
2016-11-21 16:39:17 +0200

    five clickable stars

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -20,7 +20,12 @@ class RatingControl: UIView {
     
     // MARK: Properties
     
-    var rating = 0
+    var rating = 0 {
+        // property observer
+        didSet {
+            setNeedsLayout()    // triggers layout update on rating set
+        }
+    }
     var ratingButtons = [UIButton]()
     let spacing = 5
     let starCount = 5
@@ -29,11 +34,18 @@ class RatingControl: UIView {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+
+        let filledStarImage = UIImage(named: "filledStar")
+        let emptyStarImage = UIImage(named: "emptyStar")
         
         for _ in 0..<starCount {
             let button = UIButton()
             //let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-            button.backgroundColor = UIColor.red
+            //button.backgroundColor = UIColor.red
+            button.setImage(emptyStarImage, for: .normal)
+            button.setImage(filledStarImage, for: .selected)
+            button.setImage(filledStarImage, for: [.highlighted, .selected])
+            button.adjustsImageWhenHighlighted = false
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), for: .touchDown)
             ratingButtons += [button]
             addSubview(button)
@@ -50,6 +62,9 @@ class RatingControl: UIView {
             buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
             button.frame = buttonFrame
         }
+
+        // update buttons on load
+        updateButtonSelectionStates()
     }
     
     override public var intrinsicContentSize: CGSize {
@@ -63,7 +78,17 @@ class RatingControl: UIView {
     // MARK: Button Action
     
     func ratingButtonTapped(_ button: UIButton) {
-        print("Button pressed 👍")
+        //print("Button pressed 👍")
+        // index is optional integer, but button is 100% exist so unwrap it with !
+        rating = ratingButtons.index(of: button)! + 1
+        // update buttons on rating change
+        updateButtonSelectionStates()
     }
     
+    func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
+    }
 }
